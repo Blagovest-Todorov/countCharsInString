9f65d2e3b2bb938d8c7721addfb2fc0c596040f6
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-03-06 10:04:45 +0200

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountCharsInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputStr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries); //read a string
+
+            string newInput = string.Empty;
+
+            for (int i = 0; i < inputStr.Length; i++ )
+            {
+                newInput += inputStr[i];
+            }
+
+            Dictionary<char, int> letters = new Dictionary<char, int>();
+
+            foreach (var item in newInput)
+            {
+                if (letters.ContainsKey(item))
+                {
+                    letters[item] += 1;
+                }
+                else
+                {
+                    letters.Add(item, 1);
+                }
+            }
+
+            foreach (var element in letters)
+            {
+                Console.WriteLine($"{element.Key} -> {element.Value}");
+            }
+        }
+    }
+}
